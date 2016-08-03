a8de18d8d89fd1b017477bbf28c4d28e5f406625
Daniel <danielparra93@gmail.com>
2016-08-03 19:36:56 -0400

    added comments router

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var express = require('express');
 var server = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var commentRouter = require('./routes/comments.js'); // the ./ says that you start in the same area
 
 var port = process.env.PORT || 8080;
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors());
+server.use(commentRouter); //tells the server to use this router
 
 server.get('/', function(req,res){
   res.send('success!');
